fe4d51f7da683cb761f93f39a072b02accb9b968
Tanishka-22 <tanishkashivhare22@gmail.com>
2025-12-28 18:04:28 +0530

    a

--- server/src/routes/analyticsRoutes.ts
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { getAnalyticsSummary } from '../controllers/analyticsController';
-
-const router = express.Router();
-
-router.get('/summary', getAnalyticsSummary);
-
-export default router;
